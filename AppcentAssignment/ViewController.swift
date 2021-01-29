2ad6d0aa8bc9743228dcae0f769a78bd9048f114
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-01-29 14:41:52 +0300

    Initial Commit

---
+++ AppcentAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppcentAssignment
+//
+//  Created by Batuhan Baran on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
