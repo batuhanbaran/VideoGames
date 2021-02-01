87a7b4d842229cde7f86de6b6416285d98f0be41
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-02-01 16:03:18 +0300

    first commit

---
+++ Controllers/SecondItem.swift
@@ -0,0 +1,36 @@
+//
+//  SecondItem.swift
+//  AppcentAssignment
+//
+//  Created by Batuhan Baran on 30.01.2021.
+//
+
+import UIKit
+import SDWebImage
+
+class SecondItem: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let itemImage = UserDefaults.standard.string(forKey: "item2Image"){
+            
+            let imageUrl = URL(string: itemImage)
+            
+            if let url = imageUrl{
+                
+                imageView.sd_setImage(with: url, placeholderImage: #imageLiteral(resourceName: "imagePlaceholder"))
+            }
+        }
+        
+        else{
+            
+            imageView.image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+    }
+    
+
+
+}
---
+++ Pods/Target Support Files/Pods-AppcentAssignment/Pods-AppcentAssignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppcentAssignment : NSObject
+@end
+@implementation PodsDummy_Pods_AppcentAssignment
+@end
